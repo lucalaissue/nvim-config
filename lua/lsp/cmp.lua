37401239656b5da6e816e69c19cc0cd74d7e80ff
Luca Laissue <luca@laissue.ch>
2022-09-02 15:41:24 +0200

    Remove comments

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -5,13 +5,11 @@ local lspkind = require("lspkind")
 vim.opt.completeopt = {"menu", "menuone", "preview", "noselect"}
 cmp.setup({
 	snippet = {
-		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
 			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
 	window = {
-		-- completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
 	formatting = {
@@ -21,7 +19,7 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-k>'] = cmp.mapping.select_prev_item(),
 		['<C-j>'] = cmp.mapping.select_next_item(),
-		['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<TAB>'] = cmp.mapping.confirm({ select = true })
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
