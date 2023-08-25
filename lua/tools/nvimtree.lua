64fd49dc596bb4361d80b06dc36d3804f0bb2ee6
Luca Laissue <luca@laissue.ch>
2023-08-25 11:12:11 +0200

    Fix NVIMTree config

--- lua/tools/nvimtree.lua
+++ lua/tools/nvimtree.lua
@@ -1,8 +1,7 @@
 vim.g["quit_on_open"] = 1
 require('nvim-tree').setup{
 	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false
+	hijack_netrw = true
 }
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
 
